9a2d9287763c946ef21a0cbd62f824eadd635189
Frédéric Collonval <fcollonval@users.noreply.github.com>
2024-05-06 17:41:32 +0200

    Fix unit tests

--- jupyter_server_nbmodel/tests/test_handlers.py
+++ jupyter_server_nbmodel/tests/test_handlers.py
@@ -28,25 +28,27 @@ def pending_kernel_is_ready(jp_serverapp):
 
 
 @pytest.mark.timeout(TEST_TIMEOUT)
-@pytest.mark.parametrize("snippet,output",
+@pytest.mark.parametrize(
+    "snippet,output",
     (
-        ("print('hello buddy')", '{"output_type": "stream", "name": "stdout", "text": "hello buddy\\n"}'),
-        ("a = 1", '{}'),
-        ("1 / 0", '{}')
-    )
+        (
+            "print('hello buddy')",
+            '{"output_type": "stream", "name": "stdout", "text": "hello buddy\\n"}',
+        ),
+        ("a = 1", ""),
+    ),
 )
 async def test_post_execute(jp_fetch, pending_kernel_is_ready, snippet, output):
-    # Start the first kernel
     r = await jp_fetch(
         "api", "kernels", method="POST", body=json.dumps({"name": NATIVE_KERNEL_NAME})
     )
-    kernel1 = json.loads(r.body.decode())
-    await pending_kernel_is_ready(kernel1["id"])
+    kernel = json.loads(r.body.decode())
+    await pending_kernel_is_ready(kernel["id"])
 
     response = await jp_fetch(
         "api",
         "kernels",
-        kernel1["id"],
+        kernel["id"],
         "execute",
         method="POST",
         body=json.dumps({"code": snippet}),
@@ -57,8 +59,47 @@ async def test_post_execute(jp_fetch, pending_kernel_is_ready, snippet, output):
     assert payload == {
         "status": "ok",
         "execution_count": 1,
-        "outputs": f'[{output}]',
+        "outputs": f"[{output}]",
     }
 
-    await jp_fetch("api", "kernels", kernel1["id"], method="DELETE")
-    assert response.code == 204
+    response2 = await jp_fetch("api", "kernels", kernel["id"], method="DELETE")
+    assert response2.code == 204
+
+
+@pytest.mark.timeout(TEST_TIMEOUT)
+@pytest.mark.parametrize(
+    "snippet,output",
+    (
+        (
+            "1 / 0",
+            '{"output_type": "error", "ename": "ZeroDivisionError", "evalue": "division by zero", "traceback": ["\\u001b[0;31m---------------------------------------------------------------------------\\u001b[0m", "\\u001b[0;31mZeroDivisionError\\u001b[0m                         Traceback (most recent call last)", "Cell \\u001b[0;32mIn[1], line 1\\u001b[0m\\n\\u001b[0;32m----> 1\\u001b[0m \\u001b[38;5;241;43m1\\u001b[39;49m\\u001b[43m \\u001b[49m\\u001b[38;5;241;43m/\\u001b[39;49m\\u001b[43m \\u001b[49m\\u001b[38;5;241;43m0\\u001b[39;49m\\n", "\\u001b[0;31mZeroDivisionError\\u001b[0m: division by zero"]}',  # noqa: E501
+        ),
+    ),
+)
+async def test_post_erroneous_execute(jp_fetch, pending_kernel_is_ready, snippet, output):
+    # Start the first kernel
+    r = await jp_fetch(
+        "api", "kernels", method="POST", body=json.dumps({"name": NATIVE_KERNEL_NAME})
+    )
+    kernel = json.loads(r.body.decode())
+    await pending_kernel_is_ready(kernel["id"])
+
+    response = await jp_fetch(
+        "api",
+        "kernels",
+        kernel["id"],
+        "execute",
+        method="POST",
+        body=json.dumps({"code": snippet}),
+    )
+
+    assert response.code == 200
+    payload = json.loads(response.body)
+    assert payload == {
+        "status": "error",
+        "execution_count": 1,
+        "outputs": f"[{output}]",
+    }
+
+    response2 = await jp_fetch("api", "kernels", kernel["id"], method="DELETE")
+    assert response2.code == 204
