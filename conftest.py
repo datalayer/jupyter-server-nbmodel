7b01c0835fcdf1a5a71a0acc914623a89914afb4
Darshan808 <pranishpoudel10@gmail.com>
2025-01-09 16:25:15 +0545

    add additional config

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ pytest_plugins = ("pytest_jupyter.jupyter_server","jupyter_server_ydoc.pytest_pl
 
 
 @pytest.fixture
-def jp_server_config(jp_server_config):
+def jp_server_config(jp_root_dir,jp_server_config):
     return {
         "ServerApp": {
             "jpserver_extensions": {
@@ -12,6 +12,13 @@ def jp_server_config(jp_server_config):
                 "jupyter_server_nbmodel": True,
                 "jupyter_server_fileid": True, 
             },
+            "SQLiteYStore": {"db_path": str(jp_root_dir.joinpath(".rtc_test.db"))},
+            "BaseFileIdManager": {
+                "root_dir": str(jp_root_dir),
+                "db_path": str(jp_root_dir.joinpath(".fid_test.db")),
+                "db_journal_mode": "OFF",
+            },
+            "YDocExtension": {"document_save_delay": 1},
             'IdentityProvider': {'token': ''},
             "disable_check_xsrf": True,
         },
