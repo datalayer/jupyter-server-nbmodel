cf195be9db91f953d8383076bd8a34c9fa918bce
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2025-07-18 16:35:33 +0100

    Fix build and CI (#50)
    * Fix build and CI
    
    * Lint
    
    * Downgrade eslint
    
    * Add pytest-asyncio
    
    * Fix failing test
    
    * Fix flaky tests by running them in isolation with `pytest-forked`
    
    * Restore lock file in ui-tests
    
    * Add a log for testing

--- src/plugin.ts
+++ src/plugin.ts
@@ -4,19 +4,25 @@
  * Distributed under the terms of the Modified BSD License.
  */
 
-import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
 import { INotebookCellExecutor } from '@jupyterlab/notebook';
 import { NotebookCellServerExecutor } from './executor';
 
-export const notebookCellExecutor: JupyterFrontEndPlugin<INotebookCellExecutor> = {
-  id: '@datalayer/jupyter-server-nbmodel:notebook-cell-executor',
-  description:
-    'Add notebook cell executor that uses REST API instead of kernel protocol over WebSocket.',
-  autoStart: true,
-  provides: INotebookCellExecutor,
-  activate: (app: JupyterFrontEnd): INotebookCellExecutor => {
-    return new NotebookCellServerExecutor({
-      serverSettings: app.serviceManager.serverSettings
-    });
-  }
-};
+export const notebookCellExecutor: JupyterFrontEndPlugin<INotebookCellExecutor> =
+  {
+    id: '@datalayer/jupyter-server-nbmodel:notebook-cell-executor',
+    description:
+      'Add notebook cell executor that uses REST API instead of kernel protocol over WebSocket.',
+    autoStart: true,
+    provides: INotebookCellExecutor,
+    activate: (app: JupyterFrontEnd): INotebookCellExecutor => {
+      const executor = new NotebookCellServerExecutor({
+        serverSettings: app.serviceManager.serverSettings
+      });
+      console.log('JupyterLab extension jupyter-server-nbmodel is activated!');
+      return executor;
+    }
+  };
